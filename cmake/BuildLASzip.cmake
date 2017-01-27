c5efa3979b3884e8165ba0d448e9a3abd18b62fe
Will Lucas <mevatron@gmail.com>
2017-01-27 17:57:37 -0600

    Added Debian packaging Removed ExternalProject_Add install as it makes for a wonky package :P

--- cmake/BuildLASzip.cmake
+++ cmake/BuildLASzip.cmake
@@ -7,9 +7,10 @@ ExternalProject_Add(
     PREFIX ${LASTOOLS_PREFIX}
     GIT_REPOSITORY "https://github.com/mevatron/LAStools.git"
     GIT_TAG "master"
-    INSTALL_DIR ${LASTOOLS_PREFIX}/install
+    INSTALL_DIR ""
+    INSTALL_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DCMAKE_INSTALL_PREFIX=${LASTOOLS_PREFIX}/install "${LASTOOLS_PREFIX}/src/LAStools/LASzip"
 )
 
-set(LASZIP_INCLUDE_DIRS ${LASTOOLS_PREFIX}/install/include)
-set(LASZIP_LIBRARY ${LASTOOLS_PREFIX}/install/lib/liblaszip.a)
+set(LASZIP_INCLUDE_DIRS ${LASTOOLS_PREFIX}/src/LAStools/LASzip/dll)
+set(LASZIP_LIBRARY ${LASTOOLS_PREFIX}/src/LAStools-build/src/liblaszip.a)
